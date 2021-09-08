97c06a6cf2bc8baa069657a330fb28af08b2298c
gerardmi24 <gerardmistretta@yahoo.com>
2021-09-08 00:05:02 -0400

    Just solved the event.target.value bug that keeps popping up. Need to get more comfortable with the syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+
+  onInput(value: any) {
+    // const parsedValue = parseInt(value.target.useDebugValue(value))
+    console.log('value:::', value.target.value);
+    // const parsedValue=parseInt(value.target.value)
+    // if(!isNaN(parsedValue)) {
+    //    parsedValue
+    // }
+  }
 }
